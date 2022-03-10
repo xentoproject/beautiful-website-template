2a2061bb6f76e4cfbe1f2ba59c9a8bdfd263921a
Matt <matt@spectrebot.net>
2022-03-10 16:16:46 +0000

    fix JS bugs

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ window.onscroll = function() {navScroll()};
 
 function navScroll() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementByClassName("navbar").style.;
+    document.getElementByClassName("navbar").style.backgroundFilter = "blur 15px";
   } else {
-    document.getElementByClassName("navbar").style.;
+    document.getElementByClassName("navbar").style.backgroundFilter = "none";
   }
 }
