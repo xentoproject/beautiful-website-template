6f3bd2d3bd6e1be805929bf049e855e45ed36031
Matt <matt@spectrebot.net>
2022-03-10 13:56:00 +0000

    initialise index head

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Fonts -->
+        <link SameSite="None Secure" rel="stylesheet" href="https://use.typekit.net/del7grk.css">
+
+        <!-- Bootstrap -->
+        <link SameSite="None Secure" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script SameSite="None Secure" src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+        <script SameSite="None Secure" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script SameSite="None Secure" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="style.css" />
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" SameSite="Strict" href="./asset/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" SameSite="Strict" href="./asset/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" SameSite="Strict" href="./asset/favicon/favicon-16x16.png">
+        <link rel="manifest" href="./json/site.webmanifest">
+
+        <!-- Open Graph tags -->
+        <meta property="og:title" content="Xento"/>
+        <meta property="og:description" content="Learn to code with the greatest guide in the game. Try Xento today!"/>
+        <meta property="og:image" SameSite="Strict" content="https://xento.xyz/asset/homeog.png"/>
+        <meta property="og:site_name" content="Xento"/>
+        <meta name="og:url" SameSite="Strict" content="https://xento.xyz/">
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" SameSite="Strict" content="https://xento.xyz/asset/homeog.png" />
+        <meta property="twitter:site" content="Xento" />
+
+        <title>Xento</title>
+        <meta name="description" content="Learn to code with the greatest guide in the game. Try Xento today!">
+        <meta name="keywords" content="Code, Coding, Xento, Learn to code, Programming">
+        <meta name="author" content="Matt Davison">
+
+    </head>
+
+    <body>
+
+    </body>
