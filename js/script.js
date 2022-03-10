ae500d7bea5df1069e1a057ccfcf3e9af5d2efde
Matt <matt@spectrebot.net>
2022-03-10 16:01:32 +0000

    buggy JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {navScroll()};
+
+function navScroll() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementByClassName("navbar").style.;
+  } else {
+    document.getElementByClassName("navbar").style.;
+  }
+}
