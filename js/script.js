822dcbec7353a3651e7db0d488fab73c834f9776
Matt <matt@spectrebot.net>
2022-03-21 12:26:09 +0000

    reset scroll blur

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 //navbar scroll styles
 
-window.onscroll = function() {navScroll()};
+/*window.onscroll = function() {navScroll()};
 
 function navScroll() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementByClassName("navbar").style.backgroundFilter = "blur 15px";
   } else {
     document.getElementByClassName("navbar").style.backgroundFilter = "none";
+  }
+}*/
+
+// navbar scroll blur
+var navbar = document.getElementbyClassName('bg-nav');
+
+window.onscroll = function() {stickyMenu()};
+
+function stickyMenu() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      navbar.classList.add("sticky-blur");
+  } else {
+      navbar.classList.remove("sticky-blur");
   }
 }
 
