94ecfc4ec175e255ae5a2ddfe6fa2e49ae5fa71e
Matt <matt@spectrebot.net>
2022-03-23 20:53:45 +0000

    remove unuseful comments

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//navbar scroll styles
-
-/*window.onscroll = function() {navScroll()};
-
-function navScroll() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementByClassName("navbar").style.backgroundFilter = "blur 15px";
-  } else {
-    document.getElementByClassName("navbar").style.backgroundFilter = "none";
-  }
-}*/
-
-// navbar scroll blur
-
 //open mobile nav
 
 function openNav() {
